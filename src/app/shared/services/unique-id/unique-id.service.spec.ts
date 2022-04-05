07431cf156814cf8cf29c83a0499018923207eaf
Andre Rascio Dini <andre.dini@linx.com.br>
2022-04-05 19:58:14 -0300

    Subindo beforeEach

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,18 +1,18 @@
 import { UniqueServiceId } from './unique-id.service';
 describe(UniqueServiceId.name, () => {
 
-  beforeEach(() => {
+  let service: UniqueServiceId = null;
 
+  beforeEach(() => {
+    service = new UniqueServiceId();
   });
 
   it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} should generate Id when called with prefix`, () => {
-    const service = new UniqueServiceId();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id).toContain('app-');
   });
 
   it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-    const service = new UniqueServiceId();
     const firstId = service.generateUniqueIdWithPrefix('app');
     const secondId = service.generateUniqueIdWithPrefix('app');
     expect(firstId).not.toBe(secondId);
